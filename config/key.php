e8147e719da7ad17b77bae8b7dc1a3096c20f56c
KadaJinh <admin@sorpack.cn>
2022-10-07 01:16:59 +0800

    Update key.php
    修复错误的获取地址

--- config/key.php
+++ config/key.php
@@ -4,7 +4,7 @@
 //salt值目前采用在线获取其他仓库配置
 //如有更改APP版本意愿请自行获取对应版本salt和DS算法,github上可以借鉴其他仓库代码的DS算法
 if(Config('app.online_key')){
-    $online = (new GuzzleHttp\Client())->get('https://raw.fastgit.org/Womsxd/AutoMihoyoBBS/c6a684318fcc2a1018dca88e46b3fa7d669b5003/setting.py')->getBody()->getContents();
+    $online = (new GuzzleHttp\Client())->get('https://raw.fastgit.org/Womsxd/AutoMihoyoBBS/master/setting.py')->getBody()->getContents();
     preg_match('/mihoyobbs_Salt_web = "(.*?)"/',$online,$matches);
     $salt = $matches[1];
     preg_match('/mihoyobbs_Version = "(.*?)"/',$online,$matches);
